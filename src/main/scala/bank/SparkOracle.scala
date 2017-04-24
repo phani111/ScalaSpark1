8a8a096516025241efaaf4bd7aa21b0511638bd3
spark-wc <phani111@gmail.com>
2017-04-25 00:35:57 +0530

    working

---
+++ src/main/scala/bank/SparkOracle.scala
@@ -0,0 +1,39 @@
+package bank
+
+/**
+  * Created by hadoop on 24/4/17.
+  */
+
+import java.util.Properties
+
+import org.apache.spark.sql.SparkSession
+
+object SparkOracle {
+  def main(args: Array[String]) {
+    //val spark = SparkSession.builder.master("local[*]").appName("SparkOracle").config("spark.sql.warehouse.dir", "/home/hadoop/work/warehouse").enableHiveSupport().getOrCreate()
+    val spark = SparkSession.builder.master("local[*]").appName("SparkOracle").getOrCreate()
+    val sc = spark.sparkContext
+    val sqlContext = spark.sqlContext
+    import spark.implicits._
+    import spark.sql
+    import java.util.Properties
+    val ohost = "jdbc:oracle:thin://@orcldb.cngjcoshs6ee.ap-south-1.rds.amazonaws.com:1521/ORCL"
+    val oprop = new Properties
+    oprop.setProperty("driver","oracle.jdbc.OracleDriver")
+    oprop.setProperty("user","ousername")
+    oprop.setProperty("password","opassword")
+
+    val odf = spark.read.jdbc(ohost,"DEPT",oprop)
+    val odf1 = spark.read.jdbc(ohost,"EMP",oprop)
+    //odf.show()
+    //val result = odf.where("DEPTNO > 20")
+    //result.write.csv("file:///home/hadoop/result/oracle1")
+    //result.write.json("file:///home/hadoop/result/json")
+    odf.createOrReplaceTempView("DEPT")
+    odf1.createOrReplaceTempView("EMP")
+    val result2 = spark.sql("SELECT e.DEPTNO, d.DNAME, sum(e.SAL) FROM EMP e  inner join DEPT d on e.DEPTNO = d.DEPTNO\ngroup by e.DEPTNO, d.DNAME ")
+    result2.show()
+    result2.write.csv("file:///home/hadoop/result/join")
+    spark.stop()
+  }
+}
